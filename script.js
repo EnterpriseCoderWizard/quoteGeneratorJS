b3f6e3f853a11d55b7f498d8aad7eab393b9fcdb
Edy Lopez <edilyhouse@gmail.com>
2024-08-26 18:47:51 -0600

    Commit without Refactoring

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
 let apiQuotes = [];
 
-// Show new Quote
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
 
+// Hide Loading
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+// Show new Quote
 function newQuote() {
+  loading();
   // set a random quote from apiquotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+
+  // check if author exists
+  !quote.author
+    ? (authorText.textContent = "Unknown")
+    : (authorText.textContent = quote.author);
+  // Check if quote is large
+  quote.text.length > 50
+    ? quoteText.classList.add("long-quote")
+    : quoteText.classList.remove("long-quote");
+
+  quoteText.textContent = quote.text;
+
+  complete();
 }
 
 // Get Quotes from API
 async function getQuotes() {
+  loading();
   const url = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(url);
@@ -20,6 +53,17 @@ async function getQuotes() {
   }
 }
 
-// On Load so we need to call our funtion once content is loaded
+// Tweet Quote
 
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listener
+
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On Load so we need to call our funtion once content is loaded
 getQuotes();
